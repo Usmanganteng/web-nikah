871a921098330dc2316e9e029393e130d5ef5cf1
Muhamad Aldizar Ilham <ilhamaldizar@gmail.com>
2025-03-20 21:28:45 +0700

    aasda

--- script.js
+++ script.js
@@ -27,13 +27,14 @@ document.addEventListener("DOMContentLoaded", function() {
 document.addEventListener("DOMContentLoaded", function () {
   // Firebase configuration
   const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    databaseURL: "YOUR_DATABASE_URL",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyDFBigoWysynrkva5lvD8Ghgb3NPvf79Ow",
+    authDomain: "aldizar.firebaseapp.com",
+    databaseURL: "https://aldizar-default-rtdb.firebaseio.com",
+    projectId: "aldizar",
+    storageBucket: "aldizar.appspot.com",
+    messagingSenderId: "423538284515",
+    appId: "1:423538284515:web:406f3a04e6821343f8b2d6",
+    measurementId: "G-J6R7SBS4Y0"
   };
 
   // Initialize Firebase
